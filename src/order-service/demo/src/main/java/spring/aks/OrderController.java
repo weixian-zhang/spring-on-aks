889ed942f557407f0f38249ecbd2241f231b19a6
wxz <weixianzhang@outlook.com>
2025-04-15 16:37:35 +0800

    Order service calls warehouse service by getting hostname and port from Eureka server

--- src/order-service/demo/src/main/java/spring/aks/OrderController.java
+++ src/order-service/demo/src/main/java/spring/aks/OrderController.java
@@ -2,9 +2,16 @@ package spring.aks;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 
 @RestController
 public class OrderController {
@@ -26,7 +33,7 @@ public class OrderController {
 	// private LoadBalancerClient loadBalancerClient;
 
 	@Autowired
-    private DiscoveryClient discoveryClient;
+    private EurekaClient eurekaClient;
 
 	@GetMapping("/order")
 	public String order(@RequestParam(value = "name", defaultValue = "World") String name) {
@@ -36,4 +43,21 @@ public class OrderController {
 		//discoveryClient.getInstances(name)
 		return  "order received";
 	}
+
+	@GetMapping("/order/warehouse")
+	public String order() {
+
+		Application application = eurekaClient.getApplication("warehouse-service");
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        String hostname = instanceInfo.getHostName();
+        int port = instanceInfo.getPort();
+		
+
+		RestTemplate restTemplate = new RestTemplate();
+        String warehouseServiceUrl = "http://" + hostname + ":" + port + "/warehouse";
+        ResponseEntity<String> response = restTemplate.getForEntity(warehouseServiceUrl, String.class);
+ 
+        return response.getBody();
+    }
+ 
 }
